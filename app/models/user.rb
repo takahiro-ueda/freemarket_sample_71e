dc65e2839fc8f81346b428bf2e5a8239bf8b0601
sheepdog0820 <sheepdgo1919@gmail.com>
2020-03-25 19:22:54 +0900

    ログイン、サインイン作成
    ヘッダーから移動
    ログイン、サインイン作成画面
    ウィザード形式で実装
    アドレス作成

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname,:birth_date, presence: true
+  has_one :address
+end
---
+++ db/migrate/20200325092813_create_addresses.rb
@@ -0,0 +1,18 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.string :family_name, null: false
+      t.string :first＿name, null: false
+      t.string :family_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.integer :tel, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :zip_code, null: false
+      t.string :municipality, null: false
+      t.string :address, null: false
+      t.string :building_name, null: false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
