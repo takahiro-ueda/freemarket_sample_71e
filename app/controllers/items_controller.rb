e436e94141b83496ac88d01b3916c9c45a35bfd3
fumi1182 <takafumi@ootsukatakashinoMacBook-Pro.local>
2020-03-26 21:57:38 +0900

    itemsContrillerアクション、アイテムモデル：バリテーション  routes.rb  スタイルシートのアプリケーションscss @import  追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,31 @@ class ItemsController < ApplicationController
   def index
     # @parents = Category.all.order("id ASC").limit(13)
   end
+
+  def new
+  end
+
+  def create
+  end
+  
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def item_params
+  end
+
+  def set_item
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,5 @@ class Item < ApplicationRecord
   belongs_to :brand
   has_many :item_images, dependent: :destroy
   has_many :comments, dependent: :destroy
+  validates :size, :category, :delivery, :brand, :name, :introduction, :price, :status, presence: true
 end
