e9973467bc0aa8ca772e8c235d9daab17487b31b
sheepdog0820 <sheepdgo1919@gmail.com>
2020-03-26 16:16:45 +0900

    マイページからログアウト
    マイページからのリンクを作成
    ログアウトを実装
    マイページの名前を変更
    部分テンプレートでマイページの一部を実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     post 'addresses', to: 'users/registrations#create_address'
   end
   root 'items#index'
+  resources :users, :only => [:index, :show,]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
