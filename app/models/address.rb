c8c7fd9cfe6a87396dfb33e12c9bdf3186211ef8
sheepdog0820 <sheepdgo1919@gmail.com>
2020-03-27 12:58:19 +0900

    バリテーション設定
    モデル内でバリテーション作成
    アドレスのバリテーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,5 +3,12 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :user, optional: true
   
-  validates :family_name,:first＿name,:family_name_kana,:first_name_kana,:tel,:prefecture_id,:zip_code,:municipality,:address,:building_name,presence: true
+  validates :prefecture_id,:municipality,:address,presence: true
+  validates :family_name,:first＿name, presence: true,
+            format:  {with: /\A[ぁ-んァ-ヶー一-龠]+\z/, message: "全角で入力してください"}   
+  validates :family_name_kana, :first_name_kana, presence: true,
+            format:  {with: /\A([ァ-ン]|ー)+/, message: "カタカナ以外の文字が入力されています"}   
+  validates :tel, format:  {with: /\A[0-9]+\Z/, message:"電話番号は10桁か11桁で入力してください"}
+  validates :zip_code, presence: true,
+            format:  {with: /\A\d{7}\Z/, message:"ハイフンなしで７桁の数字を入力してください"}
 end
