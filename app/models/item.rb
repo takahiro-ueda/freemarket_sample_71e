9dac17b8599ea1c1e6d510b435145a95d4817c39
fumi1182 <takafumi@ootsukatakashinoMacBook-Pro.local>
2020-04-02 14:34:25 +0900

    サイズとカテゴリをactivehashに変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :size
+  belongs_to_active_hash :category
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :payer
   belongs_to_active_hash :delivery
@@ -7,8 +9,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :trade
   belongs_to :user, optional: true
-  belongs_to :size, optional: true
-  belongs_to :category
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
   has_many :comments, dependent: :destroy
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,4 +1,9 @@
-class Size < ApplicationRecord
-  has_many :categories
-  has_many :items
+class Size < ActiveHash::Base
+  self.data = [
+      {id: 1, size: 'S'},{id: 2, size: 'M'},
+      {id: 3, size: 'L'}
+      # {id: 4, name: '24.5cm', category_id:},
+      # {id: 5, name: '25.0cm', category_id:4,6},{id: 6, name: '25.5cm', category_id:},
+      # {id: 6, name: 'サイス指定なし', category_id:},
+  ]
 end
