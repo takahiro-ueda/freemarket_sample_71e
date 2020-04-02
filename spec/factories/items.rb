3143c8951b0f4e9994ecdfec43f05d27223c77cd
TakaoTeruaki <k.teru.827@gmail.com>
2020-04-02 14:24:00 +0900

    商品出品_単体テスト2

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,22 +1,22 @@
 FactoryBot.define do
 
   factory :item do    
-    # seller_id           {"1"}  #売り手
-    # buyer_id            {"2"}  #買い手
-    # size_id             {"1"}  #サイズ
+    seller_id           {"1"}  #売り手
+    buyer_id            {"2"}  #買い手
+    size_id             {"1"}  #サイズ
     category_id         {"1"}  #カテゴリー
     status_id           {"1"}  #商品の状態
-    # brand               {"1"}  #ブランド
+    brand               {"アップル"}  #ブランド
     name                {"iPhone11"}  #商品名
-    introduction        {"iPhone11 色は黒です！状態も良いです！"}  #商品説明
-    # deal_closed_date    {"2020/3/31"}  #購買日
+    introduction        {"色は黒です！状態も良いです！"}  #商品説明
+    deal_closed_date    {"2020/3/31"}  #購買日
     price               {"50000"}  #価格
-    # value               {"1"}  #評価
+    value               {"1"}  #評価
     payer_id            {"1"}  #発送の負担
     delivery_id         {"1"}  #発送の方法
     duration_id         {"1"}  #発送までの日数
     prefecture_id       {"1"}  #発送元の地域
-    # trade_id            {"1"}  #取引の状態
+    trade_id            {"1"}  #取引の状態
   end
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,7 @@ describe Item do
   describe '#create' do
     # 1. 商品名が空では登録できないこと
     it "商品名が空では登録できないこと" do
-      item = build(:item, name: "")
+      item = build(:item, name: nil)
       item.valid?
       expect(item.errors[:name]).to include("を入力してください")
     end
@@ -21,42 +21,40 @@ describe Item do
       expect(item.errors[:price]).to include("を入力してください")
     end
 
-    # # 姓名の全角でなければ入力できない
-    # it "姓名の全角でなければ入力できない" do
-    #   address = build(:address, family_name: "test")
-    #   address.valid?
-    #   expect(address.errors[:family_name]).to include("全角で入力してください")
-    # end
-    # # 姓名がなければ入力できない
-    # it "姓名の全角であれば入力できる" do
-    #   address = build(:address, family_name_kana: nil)
-    #   address.valid?
-    #   expect(address.errors[:family_name_kana]).to include("を入力してください")
-    # end
-    # # 姓名（カナ）がなければ入力できない
-    # it "カナでなければ入力できない" do
-    #   address = build(:address, family_name_kana: nil)
-    #   address.valid?
-    #   expect(address.errors[:family_name_kana]).to include("を入力してください")
-    # end
-    # # 姓名（カナ）がカナでなければ入力できない
-    # it "姓名（カナ）がカナでなければ入力できない" do
-    #   address = build(:address, family_name_kana: "test")
-    #   address.valid?
-    #   expect(address.errors[:family_name_kana]).to include("カタカナ以外の文字が入力されています")
-    # end
-    # # 電話番号10桁11桁以外であれば入力できない
-    # it "電話番号10桁11桁以外であれば入力できない" do
-    #   address = build(:address, tel: "test")
-    #   address.valid?
-    #   expect(address.errors[:tel]).to include("電話番号は10桁か11桁で入力してください")
-    # end
-    # # 郵便番号が７桁数字以外であれば入力できない
-    # it "郵便番号が７桁数字以外であれば入力できない" do
-    #   address = build(:address, zip_code: "test")
-    #   address.valid?
-    #   expect(address.errors[:zip_code]).to include("ハイフンなしで７桁の数字を入力してください")
-    # end
+    #nameの文字が40文字以上の場合
+    it "商品名の文字が40文字以上場合" do
+      item = build(:item, name: "a" * 41)
+      item.valid?
+      expect(item.errors[:name]).to include("入力は４０文字までです。")
+    end
+
+    it "商品説明の文字が600文字以上の場合" do
+      item = build(:item, introduction: "a" * 601)
+      item.valid?
+      expect(item.errors[:introduction]).to include("入力は６００文字までです。")
+    end
+
+    #priceが9999999円以上の場合
+    it "価格が9999999円以上の場合" do
+      item = build(:item, price: 10000000)
+      item.valid?
+      expect(item.errors[:price]).to include("入力は数字のみ300〜9999999までです。")
+    end
+
+    #priceが300円以下の場合
+    it "価格が300円以下の場合" do
+      item = build(:item, price: 299)
+      item.valid?
+      expect(item.errors[:price]).to include("入力は数字のみ300〜9999999までです。")
+    end
+
+    #brandの文字が40文字以上場合
+    it "ブランドの文字が40文字以上の場合" do
+      item = build(:item, brand: "a" * 41)
+      item.valid?
+      expect(item.errors[:brand]).to include("入力は４０文字までです")
+    end
+    
   end
 end
 
