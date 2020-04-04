979ce36ba686ad60221509fbb5efa0445d86f0c2
Novita-k <myackmyack@gmail.com>
2020-04-04 10:37:39 +0900

    マイページ住所変更画面作成
    マイページuser/showにaddress/editのリンク作成。
    ルーティングにaddress追加。
    addressのviewとerrorのviewを追加。
    addressのcontroller作成。

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -1,4 +1,34 @@
 class AddressController < ApplicationController
   def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to user_path(current_user.id)
+    else
+      flash.now[:alert] = '編集に失敗しました。'
+      render :edit
+    end
+  end
+
+
+
+  private
+
+  def address_params
+    params.require(:address).permit(
+      :family_name,
+      :first_name,
+      :family_name_kana,
+      :first_name_kana,
+      :tel,
+      :prefecture_id,
+      :zip_code,
+      :municipality,
+      :address,
+      :building_name
+    ).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'address/edit'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
@@ -12,6 +11,7 @@ Rails.application.routes.draw do
   resources :users, :only => [:index, :show,]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items
+  resources :address, only: [:edit, :update]
 
   resources :credit, only: [:new, :show] do
     collection do
